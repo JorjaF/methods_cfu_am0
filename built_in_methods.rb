79aaf99d08a3fc8cb8982c2568b20531cfefb8cb
Jorja Fleming <aset284@gmail.com>
2023-02-23 18:39:56 -0700

    Inital commit

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -7,21 +7,23 @@
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
 "Hello World".downcase
-
+# this prints string as all lower case
 
 "Hello World".include?("Hello")
-
+# this means that the string includes the text in the argument
 
 "Hello World".end_with?("Hello")
-
+# this is a boolean that returns false because the string does not end with hello
 
 "Hello World".end_with?("rld")
-
+# this is a boolean that returns true because the string does end with "rld"
 
 12.even?
+# this is a boolean that returns true because 12 is an even number
 
 
 18.next
+# this is a method that returns the next value, in this case 19
 
 
 
@@ -39,6 +41,12 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+greeting = "hello"
+farewell = "good bye"
+# this returns "hellp" because it changes the last letter in the string to the next letter of the alphabet 
+greeting.next
+# this returnds "GOOD BYE" because it capitalizes all the letters in the string
+farewell.upcase
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -48,9 +56,22 @@ puts first_name.start_with?("J")
 # Include comments above each method call explaining the impact and return value of that method.
 
 
+number = 1
+# this is a boolean that will come back as false because this is not an even number 
+number.even?
+number2 = 2
+# this is a boolean that will come back as false because this is not an odd number
+number2.odd?
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+sequence1 = [1,1,2,3,5]
+sequence2 = [8,13,21,34]
+
+# this will shuffle the order of the values in the array
+sequence1.shuffle   
+# this will add all the numbers in the array to give you a total (76)
+sequence2.sum
